a92a85ef2dd3bccfb5806ac9c6c9e8cf93bea3ba
awais1365-design <awais1365@gmail.com>
2026-04-21 12:49:42 +0500

    deploy

--- launcher-portal-backend/src/app.module.ts
+++ launcher-portal-backend/src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose'; // ✅ import this
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppsModule } from './apps/apps.module';
 
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error('MONGO_URI is not defined');
+}
+
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/gamelauncher'),
-    AppsModule,
-  ],
+  imports: [MongooseModule.forRoot(mongoUri), AppsModule],
   controllers: [AppController],
   providers: [AppService],
 })
