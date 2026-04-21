738a94eb3735479342241fff0646a7ef5d97a9ff
awais1365-design <awais1365@gmail.com>
2026-04-21 12:05:53 +0500

    fix main ts

--- launcher-portal-backend/src/main.ts
+++ launcher-portal-backend/src/main.ts
@@ -5,16 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // ✅ Allow frontend to connect
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:5173', // React Vite
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     credentials: true,
   });
 
+  // ✅ Global validation (VERY IMPORTANT)
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
+      whitelist: true, // removes extra fields
+      forbidNonWhitelisted: true, // throws error if extra fields sent
+      transform: true, // converts types automatically
     }),
   );
 
