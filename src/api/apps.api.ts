6aa8bd06f97348056509a57ab80e390e32505be0
awais1365-design <awais1365@gmail.com>
2026-04-21 13:31:27 +0500

    Deploy

--- src/api/apps.api.ts
+++ src/api/apps.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // ✅ Use Railway URL from env OR fallback to localhost
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: "https://launcherportal-app-production.up.railway.app/",
 });
 
 // --------------------
